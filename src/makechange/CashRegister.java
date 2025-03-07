1a0545c6ea42f3c2851ad880709928ac469fb3f8
protosonic <ndebruin29@gmail.com>
2025-03-07 16:08:09 -0700

    Initial commit

---
+++ src/makechange/CashRegister.java
@@ -0,0 +1,16 @@
+package makechange;
+
+import java.util.Scanner;
+
+public class CashRegister {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		double amountDue;
+		double amountPaid;
+		double changeDue;
+
+	}
+
+}
