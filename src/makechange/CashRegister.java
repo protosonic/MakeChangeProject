ffb09b2fec33e8ad7a4031d77071c4d086cfd3c7
protosonic <ndebruin29@gmail.com>
2025-03-07 17:48:24 -0700

    drafted user input and display prompts

--- src/makechange/CashRegister.java
+++ src/makechange/CashRegister.java
@@ -5,12 +5,43 @@ import java.util.Scanner;
 public class CashRegister {
 
 	public static void main(String[] args) {
+		
 		Scanner sc = new Scanner(System.in);
 		
-		double amountDue;
-		double amountPaid;
-		double changeDue;
-
+		double itemCost = 0.0;
+		double amountTendered = 0.0;
+		double change = 0.0;
+		
+		// TODO: Welcome message (add quit feature?)
+		
+		// Prompt user for price of item
+		System.out.println("Enter the price of the item ($): ");
+		itemCost  = sc.nextDouble();
+		
+		// Prompt user for amount tendered
+		System.out.println("Enter the amount tendered ($): ");
+		amountTendered = sc.nextDouble();
+		
+		change = amountTendered - itemCost;
+		
+		// Display message if provided too little or exact amount
+		// - too little
+		if(change < 0) {
+			System.out.println("Error: Insufficient funds!\n"
+					+ "Recommendation: Programming can be lucrative...");
+		// - exact amount
+		} else if(change == 0) {
+			System.out.println("Exact change has been provided, therefore no change is needed"
+					+ "\n\t\t\tHave a Great Day!"
+					+ "\n\t\t\t\texiting...");
+		// Display change
+		} else {
+			System.out.printf("Change Due: %.2f\n", change);
+			// TODO: Call printChange method here
+		}
+		
 	}
+	
+	// TODO: Calculate and display the number of bills and coins to be returned 
 
 }
